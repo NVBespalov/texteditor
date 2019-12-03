6d7669acb40d89db01634dbb5b3656928970e681
bespalovnv <bespalovnv@rus-dev.com>
2019-12-03 23:02:35 +0500

    socket to edit together

--- src/server.js
+++ src/server.js
@@ -2,20 +2,14 @@ const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 io.on('connection', socket => {
-    console.log('a user connected');
-    socket.join('some room');
-    socket.on('document change', msg => {
-        io.to('some room').emit('some event');
-
-        console.log(`message: ${msg}`);
-    });
+    socket.on('room', (roomName) => {
+        socket.join(roomName);
+        io.in(roomName).emit('connected to room', 'welcome to new room');
+        socket.on('document change', msg => io.in(roomName).emit('document updated', msg));
+    })
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
-});
+http.listen(3000, () => console.log('listening on *:3000'));
