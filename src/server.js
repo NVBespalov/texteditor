4c953192a4b9696a1651e07403c47e41182c651a
bespalovnv <bespalovnv@rus-dev.com>
2019-12-06 00:11:24 +0500

    refactoring and list of documents

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const {readFile, writeFile} = require('fs');
-const { resolve } = require('path');
+const {readdir, writeFile, stat} = require('fs');
+const {resolve} = require('path');
 const documents = {};
 
 function initializeConnectionToRoom(client, documentName, userName) {
@@ -24,11 +24,11 @@ function initializeConnectionToRoom(client, documentName, userName) {
     client.on('save document', (document, next) => {
 
         writeFile(resolve(__dirname, `../data/${documentName}.html`), document, function (error) {
-           if(error) {
-               client.emit('save document error', `can not save file ${documentName}`);
-           } else {
-               client.emit('save document success');
-           }
+            if (error) {
+                client.emit('save document error', `can not save file ${documentName}`);
+            } else {
+                client.emit('save document success');
+            }
         });
     })
 
@@ -36,22 +36,43 @@ function initializeConnectionToRoom(client, documentName, userName) {
 
 app.use(express.static(__dirname + '/assets/'));
 
+const fromNodeCallback = (...[asyncFunction, ...params]) => new Promise((resolve, reject) => {
+    asyncFunction.apply(null, [...params, (err, result) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(result);
+        }
+    }])
+});
+
 io.on('connection', client => {
     const userName = client.handshake.query.userName;
-    const documentName = client.handshake.query.documentName;
-    if(!documents[documentName]) {
-        readFile(resolve(__dirname, `../data/${documentName}.html`), (error, file) => {
-            if (error) {
-                client.emit('open document error', `requested file ${documentName} not found `);
-                client.disconnect();
-            } else {
-                documents[documentName]= file.toString();
-                initializeConnectionToRoom(client,documentName, userName);
-            }
-        });
-    } else {
-        initializeConnectionToRoom(client, documentName, userName);
-    }
+    client.emit('connected');
+    client.on('get documents list', (a, b) => {
+        fromNodeCallback(readdir, resolve(__dirname, `../data/`))
+            .then((files) => Promise
+                .all(files.map(fileName => fromNodeCallback(stat, resolve(__dirname, `../data/${fileName}`)).then(stat => ({
+                    ...stat,
+                    fileName
+                })))))
+            .then(dirInfo => client.emit('documents list', dirInfo))
+            .catch(error => client.emit('get documents list error', error));
+    });
+    // const documentName = client.handshake.query.documentName;
+    // if(!documents[documentName]) {
+    //     readFile(resolve(__dirname, `../data/${documentName}.html`), (error, file) => {
+    //         if (error) {
+    //             client.emit('open document error', `requested file ${documentName} not found `);
+    //             client.disconnect();
+    //         } else {
+    //             documents[documentName]= file.toString();
+    //             initializeConnectionToRoom(client,documentName, userName);
+    //         }
+    //     });
+    // } else {
+    //     initializeConnectionToRoom(client, documentName, userName);
+    // }
 
 
 });
